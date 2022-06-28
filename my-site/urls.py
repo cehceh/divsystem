f57b6cb21fa4e7c2910d1794ba9a89d462f2b4fc
amrammer <amr.ali@tuftstudios.com>
2022-06-28 20:25:53 +0000

    Adjust main urls.py, settings.py and .gitignore files

--- my-site/urls.py
+++ my-site/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.utils.translation import gettext_lazy as _
 
-import debug_toolbar
+
 
 
 
@@ -25,6 +28,16 @@ urlpatterns = [
     # path(_('admin/'), admin.site.urls),
     path(_('admin/'), admin.site.urls),
     
-    path('__debug__/', include(debug_toolbar.urls)),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
+
+
