bf6949f057f831ed462e419626bf6e164a4f05f3
amrammer <amr.ali@tuftstudios.com>
2022-06-29 19:33:37 +0000

    Configure main urls for api router and docs, Also to work with swagger and debug tool bar on local only when debug=True

--- my-site/urls.py
+++ my-site/urls.py
@@ -20,24 +20,57 @@ from django.conf.urls.static import static
 
 from django.utils.translation import gettext_lazy as _
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from rest_framework.routers import SimpleRouter
+from rest_framework.documentation import include_docs_urls  # new
 
 
+
+API_TITLE = 'MY SITE API'  # new
+API_DESCRIPTION = 'A Web API for .......... '  # new
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
     # path(_('admin/'), admin.site.urls),
     path(_('admin/'), admin.site.urls),
     
+    path('auth/', include('apps.authentication.urls')),
+
+    path('docs/', include_docs_urls(title=API_TITLE,
+                                    description=API_DESCRIPTION)),  # new
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+router = SimpleRouter()
+
+# you must put the next line here in the (Main URL)
+urlpatterns += router.urls
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
+    
+        path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        path('swagger-docs/', schema_view),  #  new
     ]
-
 
