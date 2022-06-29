51584bfc6e6a0b8b617f10e5be5af844387f5f0b
amrammer <amr.ali@tuftstudios.com>
2022-06-29 19:27:48 +0000

    Register CustomUsermodel to admin site

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CustomUser
+
+
 
 # Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in CustomUser._meta.fields]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
