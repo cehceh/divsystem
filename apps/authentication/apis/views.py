92fe07a856e04a48de0c3468b147d4eefab9c9ff
amrammer <amr.ali@tuftstudios.com>
2022-06-30 18:13:53 +0000

    No necessary changes

--- apps/authentication/apis/views.py
+++ apps/authentication/apis/views.py
@@ -1,10 +1,3 @@
-# from rest_framework.renderers import (
-#                                         HTMLFormRenderer, 
-#                                         JSONRenderer, 
-#                                         BrowsableAPIRenderer,
-#                                     )
-# from rest_framework import views
-
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
@@ -13,33 +6,27 @@ from dj_rest_auth.registration.views import RegisterView
 
 from dj_rest_auth.views import LoginView
 
-# from .serializers import DetailUserSerializer
-
 
 class MyCustomLogin(LoginView):
     '''
-        Managing login process
+        :Managing login process
     '''
 
     def get_response(self):
         base_response = super(MyCustomLogin, self).get_response()
-        # data = {
-        #     'phone_number': self.user.phone_number,
-        # }
+
         #* here you can get specific fields in the login response 
         base_response.data['user'] = {
             'phone_number': self.user.phone_number,
             #* here you can add some fields if you wish to get more in response
         }
-        # base_response.data.update(data)
-        # base_response.data['user']['phone_number'] = self.user.phone_number
 
         return base_response
 
 
 class MyCustomRegister(RegisterView):
     '''
-        Managing registration process
+        :Managing registration process
     '''
     def create(self, request, *args, **kwargs):    
         base_response = super(MyCustomRegister, self).create(request, *args, **kwargs)
@@ -55,72 +42,11 @@ class MyCustomRegister(RegisterView):
         return base_response
 
 
-# class DetailUserView(views.APIView):
-#     permission_classes = [IsAuthenticated]
-#     serializer_class = DetailUserSerializer
-#     renderer_classes = (BrowsableAPIRenderer, JSONRenderer, HTMLFormRenderer)
-
-#     def get_queryset(self, *args, **kwargs):
-#         user=self.request.user
-#         qs = CustomUser.objects.filter(
-#             id=user.id
-#             ).values(
-#                 'id',
-#                 'first_name', 
-#                 'last_name',
-#                 'birth_date',
-#                 'country_code',
-#                 'phone_number',
-#                 'gender'
-#             ).first()
-        
-#         return qs
-    
-#     def get(self,request, *args, **kwargs):
-#         base_response = self.get_queryset()
-#         return Response(base_response)
-    
-#     def post(self,request, *args, **kwargs):
-#         initial_data = self.get_queryset()
-#         data=request.data
-#         user=self.request.user
-#         serializer = self.serializer_class(data=data)
-#         if serializer.is_valid():
-#             # email = data['email'] or initial_data['email'] 
-#             phone_number = data['phone_number'] or initial_data['phone_number']
-#             username = data['username'] or initial_data['username']
-#             first_name = data['first_name'] or initial_data['first_name']
-#             last_name = data['last_name'] or initial_data['last_name']
-            
-#             qs = CustomUser.objects.filter(id=request.user.id)
-#             password=qs.first().password[:10]
-#             # qs.update(email=email,phone_number=phone_number,username=username)
-            
-#             base_response = self.get_queryset()
-#             return Response(base_response)
-#         else:
-#             base_response= Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#             return base_response
-
 from rest_framework_simplejwt.views import TokenViewBase
-from .serializers import CustomObtainSerializer #TokenObtainLifetimeSerializer, TokenRefreshLifetimeSerializer
+from .serializers import CustomObtainSerializer 
 
 
 class TokenObtainPairView(TokenViewBase):
-
     serializer_class = CustomObtainSerializer
 
-
-# class TokenObtainPairView(TokenViewBase):
-#     """
-#         Return JWT tokens (access and refresh) for specific user based on username and password.
-#     """
-#     serializer_class = TokenObtainLifetimeSerializer
-
-
-# class TokenRefreshView(TokenViewBase):
-#     """
-#         Renew tokens (access and refresh) with new expire time based on specific user's access token.
-#     """
-#     serializer_class = TokenRefreshLifetimeSerializer
 
--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from dj_rest_auth.views import LogoutView
+from .apis.views import TokenObtainPairView #, TokenRefreshView
 
 from django.conf import settings
 
@@ -15,9 +16,11 @@ urlpatterns = [
 
     # path("user/", DetailUserView.as_view(), name="edit_user"),
     
-    path('register/', MyCustomRegister.as_view(), name='custom_rest_register'),
-    path('login/', MyCustomLogin.as_view(), name='custom_rest_login'),
+    path('register/', MyCustomRegister.as_view(), name='custom_register'),
+    path('login/', MyCustomLogin.as_view(), name='custom_login'),
     path('logout/', LogoutView.as_view()),
+    
+    path('token/obtain/', TokenObtainPairView.as_view(), name='token_obtain'),
 
 ]
 
@@ -25,14 +28,11 @@ if getattr(settings, 'REST_USE_JWT', False):
     from rest_framework_simplejwt.views import TokenVerifyView
 
     from dj_rest_auth.jwt_auth import get_refresh_view
-    from .apis.views import TokenObtainPairView #, TokenRefreshView
 
     urlpatterns += [
         path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
         path('token/refresh/', get_refresh_view().as_view(), name='token_refresh'),
 
-        path('token/obtain/', TokenObtainPairView.as_view(), name='token-obtain'),
-        # path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
     ]
 
 
