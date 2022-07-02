052b55de8e51f93d1cba0e84e072689b8b44005c
cehceh <aboobida2002@gmail>
2022-07-02 01:13:06 +0000

    Using Base64ImageField for avatar field

--- apps/authentication/apis/serializers.py
+++ apps/authentication/apis/serializers.py
@@ -12,6 +12,59 @@ from django.contrib.auth import authenticate
 
 from ..models import  CustomUser
 
+import base64
+from django.core.files.base import ContentFile
+
+
+class Base64ImageField(serializers.ImageField):
+    """
+    A Django REST framework field for handling image-uploads through raw post data.
+    It uses base64 for encoding and decoding the contents of the file.
+
+    Heavily based on
+    https://github.com/tomchristie/django-rest-framework/pull/1268
+
+    Updated for Django REST framework 3.
+    """
+
+    def to_internal_value(self, data):
+        from django.core.files.base import ContentFile
+        import base64
+        import six
+        import uuid
+
+        # Check if this is a base64 string
+        if isinstance(data, six.string_types):
+            # Check if the base64 string is in the "data:" format
+            if 'data:' in data and ';base64,' in data:
+                # Break out the header from the base64 content
+                header, data = data.split(';base64,')
+
+            # Try to decode the file. Return validation error if it fails.
+            try:
+                decoded_file = base64.b64decode(data)
+            except TypeError:
+                self.fail('invalid_image')
+
+            # Generate file name:
+            file_name = str(uuid.uuid4())[:12] # 12 characters are more than enough.
+            # Get the file name extension:
+            file_extension = self.get_file_extension(file_name, decoded_file)
+
+            complete_file_name = "%s.%s" % (file_name, file_extension, )
+
+            data = ContentFile(decoded_file, name=complete_file_name)
+
+        return super(Base64ImageField, self).to_internal_value(data)
+
+    def get_file_extension(self, file_name, decoded_file):
+        import imghdr
+
+        extension = imghdr.what(file_name, decoded_file)
+        extension = "jpg" if extension == "jpeg" or extension == 'png'  else extension
+
+        return extension
+
 
 class CustomRegisterSerializer(RegisterSerializer):
     username = None #serializers.CharField(max_length=20, required=False, write_only=True)
@@ -30,7 +83,10 @@ class CustomRegisterSerializer(RegisterSerializer):
         ('female', 'Female'),
     )
     gender = serializers.ChoiceField(choices=CHOICES)
-    avatar = serializers.ImageField(required=True)
+    # avatar = serializers.ImageField(required=True)
+    avatar = Base64ImageField(
+        max_length=None, use_url=True, required=True
+    )
     # birth_date = serializers.CharField(required=True)
     birth_date = serializers.DateField(format="%Y-%m-%d", input_formats=['%Y-%m-%d',], required=True)
     
@@ -156,7 +212,7 @@ class CustomLoginSerializer(LoginSerializer):
 
 ## For task(3) 
 
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenVerifySerializer
 
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -215,17 +271,38 @@ class CustomObtainSerializer(TokenObtainPairSerializer):
 
         return data
 
-    # @classmethod
-    # def get_token(cls, user):
-    #     token = super().get_token(user)
+## Here is the solution of the Task(3)
+class TokenAPISerializer(TokenVerifySerializer):
+    phone_number = serializers.CharField(required=True)
+    status = serializers.CharField(required=True)
 
-    #     # Add custom claims
-    #     token['phone_number'] = user.phone_number
-    #     token['status'] = user.status
-    #     # ...
-    #     print('TOKEN: ', token, 'ACCESS: ', token['phone_number'])
-    #     return token
-    
+    def validate(self, attrs):
+        # data = super().validate(attrs)
+        token = str(attrs['token'])      #UntypedToken(attrs["token"])
+        phone = str(attrs['phone_number'])
+        # status = str(attrs['status'])
+        
+        # from rest_framework.authtoken.models import Token
+        # user_obj = CustomUser.objects.get(phone_number=phone)
+        # user_token = Token.objects.get_or_create(user_id=user_obj.id)
+
+        # match = Token.objects.filter(user_id=user_obj.id).exists()
+        # if match:
+        #     token_obj = Token.objects.get(user_id=user_obj.id)
+        # else:
+        #     token_obj = []
+
+        # print('Token: ', user_token[1],)
+        # if user_token[1]:
+        return {
+            'token': str(token),
+            'phone_number': str(phone),
+            # 'status': str(status),
+        }
+        # else:
+        #     raise serializers.ValidationError('Credential does not match')
+        
+        
 
 
 ## Another solution for task(3)
